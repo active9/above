7a7cf451ff80dbc419b63309974d2e139bac5365
talkquazi <talkquazi@gmail.com>
2016-08-18 11:53:25 -0400

    cleanup and push to 1.0.0

--- examples/average.js
+++ examples/average.js
@@ -1,12 +1,12 @@
 var above = require('../lib/above.js');
 
 function testFunction(data, datab) {
-	console.log('I am testFunction', data, datab);
+  console.log('I am testFunction', data, datab);
 }
 
 testFunction = above(testFunction, function(func, argz, argy) {
-	console.log('This was called before testFunction');
-	func(argz, argy);
+  console.log('This was called before testFunction');
+  func(argz, argy);
 });
 
 testFunction("Hello", "World");
--- examples/complex.js
+++ examples/complex.js
@@ -1,14 +1,14 @@
 var above = require('../lib/above.js');
 
 function testFunction(fn) {
-	fn(console.log('I am testFunction'));
+  fn(console.log('I am testFunction'));
 }
 
 testFunction = above(testFunction, function(func, argz, cb) {
-	console.log('This was called before testFunction');
-	func(cb);
+  console.log('This was called before testFunction');
+  func(cb);
 });
 
 testFunction("test", function(argz) {
-	console.log("I am a callback.");
+  console.log("I am a callback.");
 });
